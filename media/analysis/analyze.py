ec40d30d619251bf01c87234a06d65d89d6382c3
Peter Schultz <kaedenn@gmail.com>
2015-07-09 18:49:36 -0700

    Improved analyze.py and updated analysis3.txt

--- media/analysis/analyze.py
+++ media/analysis/analyze.py
@@ -2,15 +2,26 @@
 
 import sys
 
-labels = [['START-TRACK']]
-last_label = 0
-for l in sys.stdin:
-    if '\t' in l:
-        b, e, l = l.strip().split(None, 2)
+def process_fileobj(fileobj):
+    labels = []
+    labels.append(['START-TRACK'])
+    labels.extend([process_line(l) for l in fileobj])
+    labels.append(['END-TRACK'])
+    return labels
+
+def process_line(line):
+    if '\t' in line:
+        b, e, l = line.strip().split(None, 2)
         begin = float(b)
         end = float(e)
-        labels.append([int(begin*1000), int(end*1000), l])
-labels.append(['END-TRACK'])
+        return [int(begin*1000), int(end*1000), l]
+    return []
 
-for label in labels:
-    print ", ".join(str(i) for i in label)
+fileobjs = [sys.stdin]
+if len(sys.argv) > 1:
+    fileobjs = [open(arg) for arg in sys.argv[1:]]
+for fileobj in fileobjs:
+    labels = process_fileobj(fileobj)
+    for label in labels:
+        print ", ".join(str(l) for l in label)
+
